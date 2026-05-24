b03eaad5c1bb1e11b6fb21bfd4b41d088323a56d
Varshitha ellenki <varshithaellenki@gmail.com>
2026-05-24 23:07:22 +0530

    chore: initial project setup with Spring Initializr

---
+++ codebase-explainer/src/main/java/com/sonu/codebase_explainer/CodebaseExplainerApplication.java
@@ -0,0 +1,13 @@
+package com.sonu.codebase_explainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebaseExplainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebaseExplainerApplication.class, args);
+	}
+
+}
---
+++ codebase-explainer/src/test/java/com/sonu/codebase_explainer/CodebaseExplainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonu.codebase_explainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebaseExplainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
